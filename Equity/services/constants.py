472a8339b05600e1040ab8bedc4e4b65cbfc7fb3
imaayush <aayuhtiwari41@gmail.com>
2017-12-28 18:38:45 +0530

    Rename Equity to equity

--- Equity/services/constants.py
+++
@@ -1,7 +0,0 @@
-
-
-FIELDS_NAMES = ["SC_CODE", "SC_NAME", "OPEN", "HIGH", "LOW", "CLOSE"]
-SHOW_NUMBER_COMPANIES = 10
-USER_AGENT = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.78 Safari/537.36"
-START_URL = "http://www.bseindia.com/markets/equity/EQReports/BhavCopyDebt.aspx?expandable=3"
-BASE_URL = "http://www.bseindia.com/markets/equity/EQReports/"
